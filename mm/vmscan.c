886a67fe12e2ececd3664ab1016d744e14ab8b71
zeeshan <zeeshanhussain12@gmail.com>
2016-01-05 21:33:13 +0530

    Fix Build
    * Also revert previous commits of lmk

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -132,7 +132,7 @@ struct mem_cgroup_zone {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 40;
+int vm_swappiness = 60;
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
@@ -2885,7 +2885,10 @@ static int kswapd(void *p)
 		}
 	}
 
+	tsk->flags &= ~(PF_MEMALLOC | PF_SWAPWRITE | PF_KSWAPD);
 	current->reclaim_state = NULL;
+	lockdep_clear_current_reclaim_state();
+
 	return 0;
 }
 
